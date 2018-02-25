2382d3d29858dc8935fbc865a14234ffa5e4a4ae
shouwj <1132331056@qq.com>
2018-02-25 10:35:08 +0800

    better solution

--- src/com/swj/dp/RangeSumQuery2DImmutable.java
+++ src/com/swj/dp/RangeSumQuery2DImmutable.java
@@ -10,7 +10,8 @@ import java.util.Arrays;
  */
 public class RangeSumQuery2DImmutable {
 
-    private final int[][] horizontal;
+    /*private final int[][] horizontal;
+
     public RangeSumQuery2DImmutable(int[][] matrix){
         horizontal = matrix;
         for (int i = 0; i < matrix.length; i ++){
@@ -30,8 +31,28 @@ public class RangeSumQuery2DImmutable {
             }
         }
         return result;
+    }*/
+
+    private  int[][] dp;
+    public RangeSumQuery2DImmutable(int[][] matrix){
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i ++){
+            for (int j = 1; j <= n; j ++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1 ] - dp[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
     }
 
+    public int sumRegion(int row1, int col1, int row2, int col2){
+        return dp[row2 + 1][col2 + 1] - dp[row2 + 1][col1] - dp[row1][col2 + 1] + dp[row1][col1];
+    }
     public static void main(String[] args) {
         RangeSumQuery2DImmutable test = new RangeSumQuery2DImmutable(new int[][]{
                 {3, 0, 1, 4, 2},
