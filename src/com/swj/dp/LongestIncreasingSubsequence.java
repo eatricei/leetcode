6bc6eab97c818a8feff9f0e0d0c688405804efe3
shouwj <1132331056@qq.com>
2018-06-01 17:04:30 +0800

    new idea

--- src/com/swj/dp/LongestIncreasingSubsequence.java
+++ src/com/swj/dp/LongestIncreasingSubsequence.java
@@ -37,7 +37,22 @@ public class LongestIncreasingSubsequence {
         }
         return res;
     }
+
+    public int test(int[] nums){
+        int len = nums.length;
+        int[] dp = new int[len];
+        int res = 0;
+        for (int i=0;i<len;i++){
+            for (int j=0;j<=i;j++){
+                if (nums[j]<nums[i]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+            res = Math.max(dp[i], res);
+        }
+        return res+1;
+    }
     public static void main(String[] args) {
-        System.out.println(new LongestIncreasingSubsequence().lengthOfLIS(new int[]{10,9,2,5,3,4}));
+        System.out.println(new LongestIncreasingSubsequence().test(new int[]{10,9,2,5,3,7,101,18}));
     }
 }
